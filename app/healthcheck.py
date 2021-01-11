17d6d91b6696eb063970a815a745f50f478abccc
Christian Barra <me@christianbarra.com>
2021-01-11 19:40:15 +0100

    Fix typo

--- app/healthcheck.py
+++ app/healthcheck.py
@@ -1,7 +1,8 @@
-from django.http import JsonResponse
 import os
 
+from django.http import JsonResponse
+
 def healthcheck(request):
-    """App health check, return OK is the app is alive"""
+    """App health check, return OK if the app is alive"""
     version = os.environ.get("GIT_HASH")
     return JsonResponse({'version': version, 'status': 'OK'})
